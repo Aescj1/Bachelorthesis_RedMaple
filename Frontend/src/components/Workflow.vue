9237152d27fbdb844c1a2638d338f2ce9351f49c
aescj1 <janphilippe.aeschimann@students.bfh.ch>
2019-03-21 06:51:24 +0100

    workflow view

--- Frontend/src/components/Workflow.vue
+++ Frontend/src/components/Workflow.vue
@@ -13,7 +13,7 @@
 
                 <v-flex d-flex>
                     <v-card color="blue lighten-2" dark>
-                    <v-card-title primary class="title">Sequenzierungslauf</v-card-title>
+                    <v-card-title primary class="title">Lauf</v-card-title>
                     <v-card-text>{{ lorem }}</v-card-text>
                     </v-card>
                 </v-flex>
@@ -38,19 +38,30 @@
             </v-layout>
       </v-flex>
 
-        <v-flex d-flex xs2 sm2 md2 xl2 lg2  >
-            <v-layout row wrap>
-                <v-flex d-flex>
-                    <v-btn  dark large color="black">
-                        <v-icon >Test</v-icon>
+        <v-flex d-flex xs2 sm2 md2 xl2 lg2 align-content-center>
+            <v-layout row wrap >
+             <v-item-group class="item-group">
+                <v-flex>
+                    <v-btn class="processButton" id="first" fab dark large color="purple">
+                      G
                      </v-btn>
                 </v-flex>
-
-                <v-flex d-flex>
-                    <v-btn  dark large color="grey">
-                        <v-icon >Test</v-icon>
+                <v-flex>
+                    <v-btn class="processButton" fab dark large color="red">
+                        E
                      </v-btn>
                 </v-flex>
+                <v-flex>
+                    <v-btn class="processButton" fab dark large color="blue">
+                        L
+                     </v-btn>
+                </v-flex>
+                <v-flex>
+                    <v-btn class="processButton" fab dark large color="green">
+                        S
+                     </v-btn>
+                </v-flex>
+                </v-item-group>
             </v-layout>
       </v-flex>
 
@@ -71,5 +82,17 @@
 <style>
 .workTile{
     height:-webkit-fill-available !important;
+}
+.item-group{
+  width:-webkit-fill-available !important;
+  text-align: center;
+}
+.processButton{
+  font-size: 3rem;
+  margin-bottom: 50px;
+}
+#first{
+  margin-top: 9rem;
+
 }
 </style>
